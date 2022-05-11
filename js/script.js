06fe7942d88964f96012e690bfd3f4e0e6580843
SithumDev07 <sithum.dashantha@gmail.com>
2022-05-11 05:43:24 +0530

    release 02

--- js/script.js
+++ js/script.js
@@ -1,56 +1,72 @@
-const el = document.querySelector('.title')
+// Variables
+const el = document.querySelector(".title");
 
-let elWidth = el.offsetWidth
-let windowWidth = window.innerWidth
+// Variables ~ Widths
+let elWidth = el.offsetWidth;
+let windowWidth = window.innerWidth;
 
-let mouseX = 0
-let prevMouseX = 0
+// Variables ~ Mouse
+let mouseX = 0;
+let prevMouseX = 0;
 
-let skewTarget = 0
-let translateTarget = 0
+// Target: value we want to animate to
+let skewTarget = 0;
+let translateTarget = 0;
 
-let skewWithEasing = 0
-let translateWithEasing = 0
+// WithEasing: value we use to animate
+let skewWithEasing = 0;
+let translateWithEasing = 0;
 
-let skewEasingFactor = 0
-let translateEasingFactor = 0
+// EasingFactor: determines how quick the animation/interpolation goes
+let skewEasingFactor = 0.1;
+let translateEasingFactor = 0.05;
 
-window.addEventListener('mousemove', handleMouseMove)
-window.addEventListener('resize', handleWithResize)
+// Events
+window.addEventListener("mousemove", handleMouseMove);
+window.addEventListener("resize", handleWindowResize);
 
-function handleMouseMove (e) {
-	mouseX = e.pageX
+// Functions
+function handleMouseMove(e) {
+  mouseX = e.pageX;
 }
 
-function handleWithResize (e) {
-	elWidth = el.offsetWidth
-	windowWidth = window.innerWidth
+function handleWindowResize(e) {
+  elWidth = el.offsetWidth;
+  windowWidth = window.innerWidth;
 }
 
-function lerp (start, end, factor) {
-	return ( 1 - factor ) * start + factor * end
+function lerp(start, end, factor) {
+  return (1 - factor) * start + factor * end;
 }
 
-function animateMe () {
-	skewTarget = mouseX - prevMouseX
-	prevMouseX = mouseX
+function animateMe() {
+  // Get difference between current and previous mouse position
+  skewTarget = mouseX - prevMouseX;
+  prevMouseX = mouseX;
 
-	translateTarget = ( elWidth - windowWidth ) / windowWidth * mouseX - 1
-	skewWithEasing = lerp( skewWithEasing, skewTarget, skewEasingFactor )
-	skewWithEasing = Math.min( Math.max( parseInt( skewWithEasing ), -75 ), 75 )
+  // Calc how much we need to translate our el
+  translateTarget = (elWidth - windowWidth) / windowWidth * mouseX * -1;
 
-	translateWithEasing = lerp(
-		translateWithEasing,
-		translateTarget,
-		translateEasingFactor
-	)
+  // Ease between start and target values (skew)
+  skewWithEasing = lerp(skewWithEasing, skewTarget, skewEasingFactor);
 
-	el.getElementsByClassName.transform = `
-		translateX(${ translateWithEasing }px)
-		skew(${ skewWithEasing }deg)
-	`
+  // Limit our skew to a range of 75 degrees so it doesn't "over-skew"
+  skewWithEasing = Math.min(Math.max(parseInt(skewWithEasing), -75), 75);
 
-	window.requestAnimationFrame(animateMe)
+  // Ease between start and target values (translate)
+  translateWithEasing = lerp(
+  translateWithEasing,
+  translateTarget,
+  translateEasingFactor);
+
+
+  el.style.transform = `
+    translateX(${translateWithEasing}px)
+    skewX(${skewWithEasing}deg)
+  `;
+
+  // RAF
+  window.requestAnimationFrame(animateMe);
 }
 
-window.requestAnimationFrame(animateMe)
+window.requestAnimationFrame(animateMe);
